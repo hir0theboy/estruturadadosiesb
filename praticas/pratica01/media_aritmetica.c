30684877efaec8802e550b4aeb3b559cde30d288
hir0theboy <lipealves2345meia78@gmail.com>
2026-03-13 10:41:32 -0300

    feat: conclui pratica01. Fecha 01

--- praticas/pratica01/media_aritmetica.c
+++ praticas/pratica01/media_aritmetica.c
@@ -3,7 +3,7 @@
 /*media_artimetica.c: Ler um número inteiro n e calcular a média
 aritmética de n números informados pelo usuário. O valor máximo permitido para n é 100.*/
 
-int main() {
+/*int main() {
     int n, i;
     float numero, soma, media;
     printf("digite a quantidade de numeros (max 100): ");
@@ -24,6 +24,44 @@ int main() {
     media = soma/n;
 
     printf("a media foi: %.2f\n", media);
+
+    return 0;
+}*/
+
+float calcular_media(int n, float numeros[]) {
+    if (n <= 0 || n > 100) {
+        return 0.0f;
+    }
+
+    float soma = 0.0f;
+
+    for (int i = 0; i < n; i++) {
+        soma += numeros[i];
+    }
+
+    return soma / n;
+}
+
+int main() {
+
+    float numeros1[] = {2, 4, 6};
+    float media = calcular_media(3, numeros1);
+    printf("n = 3, media = %.1f => %i\n", media, media == 4.0f);
+
+    float numeros2[] = {10, 10, 10, 10};
+    media = calcular_media(4, numeros2);
+    printf("n = 4, media = %.1f => %i\n", media, media == 10.0f);
+
+    float numeros3[] = {5};
+    media = calcular_media(1, numeros3);
+    printf("n = 1, media = %.1f => %i\n", media, media == 5.0f);
+
+    float numeros4[] = {1, 2, 3, 4, 5};
+    media = calcular_media(5, numeros4);
+    printf("n = 5, media = %.1f => %i\n", media, media == 3.0f);
+
+    media = calcular_media(0, numeros4);
+    printf("n = 0, media = %.1f => %i\n", media, media == 0.0f);
 
     return 0;
 }
--- praticas/pratica01/potencia.c
+++ praticas/pratica01/potencia.c
@@ -2,6 +2,8 @@
 /*potencia.c: Ler uma base e um expoente inteiro positivo e calcular
 a potência da base elevada ao expoente sem usar funções prontas.*/
 
+
+/*
 int main() {
     int base, expoente, i;
     int resultado = 1;
@@ -17,6 +19,38 @@ int main() {
     }
 
     printf("Resultado: %d\n", resultado);
+
+    return 0;
+}*/
+
+int calcular_potencia(int base, int expoente){
+    int resultado = 1;
+
+    for(int i = 1; i <= expoente; i++){
+        resultado = resultado * base;
+    }
+
+    return resultado;
+}
+
+int main(){
+
+    int resultado = 0;
+
+    resultado = calcular_potencia(2, 3);
+    printf("base = 2, expoente = 3, resultado = %d => %i\n", resultado, resultado == 8);
+
+    resultado = calcular_potencia(5, 0);
+    printf("base = 5, expoente = 0, resultado = %d => %i\n", resultado, resultado == 1);
+
+    resultado = calcular_potencia(3, 2);
+    printf("base = 3, expoente = 2, resultado = %d => %i\n", resultado, resultado == 9);
+
+    resultado = calcular_potencia(10, 1);
+    printf("base = 10, expoente = 1, resultado = %d => %i\n", resultado, resultado == 10);
+
+    resultado = calcular_potencia(4, 4);
+    printf("base = 4, expoente = 4, resultado = %d => %i\n", resultado, resultado == 256);
 
     return 0;
 }
